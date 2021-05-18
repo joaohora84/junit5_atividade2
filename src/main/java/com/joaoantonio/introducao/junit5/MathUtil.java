793d83c79b5e3d8cc3ea7305cff54946d7d1bfe0
joaoantoniodahora <joaohora84@gmail.com>
2021-05-17 21:46:00 -0300

    Primeiro projeto Junit5 - Atividade 3

--- src/main/java/com/joaoantonio/introducao/junit5/MathUtil.java
+++ src/main/java/com/joaoantonio/introducao/junit5/MathUtil.java
@@ -1,3 +1,5 @@
+package com.joaoantonio.introducao.junit5;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,12 +14,30 @@ public class MathUtil {
     
     public static int mdc(int a, int b){
         
+        a = Math.abs(a);
+        b = Math.abs(b);
+        
+        //Propriedade6
+        final int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior;
+        
+        //Propriedade1
         if(b > 0 && a % b == 0){
             
             return b;
             
         }
+        
+        //Propriedade3
+        if(b == 0) {
+            return Math.abs(a);
+        }
          
+        //Propriedade5
+        if(a % b != 0) {
+            return 1;
+        }
         
         return -1;
         
--- src/test/java/MathUtilTest.java
+++ src/test/java/MathUtilTest.java
@@ -4,8 +4,10 @@
  * and open the template in the editor.
  */
 
+import com.joaoantonio.introducao.junit5.MathUtil;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static com.joaoantonio.introducao.junit5.MathUtil.mdc;
 
 /**
  *
@@ -35,6 +37,87 @@ class MathUtilTest {
     }
     
     @Test
+    void textMdcParesP2(){
+        final int a = 16;
+        final int b = 8;
+        final int divisor = 4;
+        final int esperado = 0;
+        final int obtido = MathUtil.mdc(a, b);
+        //assertEquals(esperado, obtido % divisor);
+        assertTrue(obtido % divisor == 0);
+    }
+    
+    @Test
+    void textMdcAPositivoP3(){
+        final int a = 15;
+        final int b =0;
+        final int esperado = 15;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+   
+    @Test
+    void testMdcANegativoP3(){
+        final int a = -30;
+        final int b = 0;
+        final int esperado = 30;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcMParP4(){
+        final int a = 9;
+        final int b = 3;
+        final int m = 2;
+        final int esperado = MathUtil.mdc(a*m, b*m);
+        final int obtido = m * MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcP5(){
+        final int a = 7;
+        final int b = 3;
+        final int c = 5;
+        final int esperado = mdc(a*b, c);
+        final int obtido = mdc(b,c);
+        System.out.println("Esperado: " + esperado + " Obtido: " + obtido );
+        assertEquals(esperado, obtido);
+    }
+    
+     @Test
+    void testMdcP5Resultado1(){
+        final int a = 7;
+        final int b = 3;
+        final int c = 5;
+        final int esperado = 1;
+        final int obtido = mdc(b,c);
+        System.out.println("Esperado: " + esperado + " Obtido: " + obtido );
+        assertEquals(esperado, obtido);
+       
+    }
+    
+    @Test
+    void testMdcP6(){
+        final int a = 8;
+        final int b = 2;
+        final int esperado = mdc(a,b);
+        final int obtido = mdc(b,a);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcP7TudoNegativo(){
+        final int a = -12;
+        final int b = -6;
+        final int esperado = 6;
+        final int obtido = mdc(a,b);
+        assertEquals(esperado, obtido);
+    }
+    
+    /*
+    @Test
     void testMdcOutroBP1(){
         final int a = 9;
         final int b = -3;
@@ -50,5 +133,5 @@ class MathUtilTest {
         final int esperado = b;
         final int obtido = MathUtil.mdc(a, b);
         assertEquals(esperado, obtido);
-    }
+    }*/
 }
